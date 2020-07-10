9a4c53655e81efa1944add5ec159de738c210fb3
Drakonkinst <drakonkinstmc@gmail.com>
2020-07-10 15:26:22 -0700

    Added singleton Scene Manager
    - Tracks player and world dimensions

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public static SceneManager Instance;
+    
+    public GameObject player;
+    public GameObject ground;
+    public Vector2 worldCenter = new Vector2(0, 0);
+    [System.NonSerialized]
+    public Transform playerTransform;
+    [System.NonSerialized]
+    public Rigidbody playerRb;
+    [System.NonSerialized]
+    public float worldWidth;
+    [System.NonSerialized]
+    public float worldLength;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Instance != null && Instance != this) {
+            Destroy(gameObject);
+        }
+        Instance = this;
+        
+        playerTransform = player.transform;
+        playerRb = player.GetComponent<Rigidbody>();
+        Vector3 size = ground.GetComponent<MeshRenderer>().bounds.size;
+        worldWidth = size.x;
+        worldLength = size.z;
+        Debug.Log(worldWidth + " x " + worldLength);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
