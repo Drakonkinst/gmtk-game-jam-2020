50b9b7f171f518a0aef02c2746d4f4735ec92500
Drinks <idrinksfire@gmail.com>
2020-07-10 15:37:22 -0700

    Enabled bullet movement and spawning
    Bullets are temporarily spawned using Space. They are spawned in the rotation of the player and will fly straight. They destroy themselves on collision with another object.

---
+++ Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    public float speed = 1;
+    private Rigidbody myRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        myRb.AddForce(Vector3.forward * 250 * speed * Time.deltaTime, ForceMode.Force);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag != "Player")
+        {
+            Debug.Log("Bullet Collision with " + collision.gameObject.name);
+            Destroy(gameObject); // When I collide with something, destroy myself
+        } 
+    }
+}
---
+++ Assets/Scripts/zzTEMPBulletTester.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zzTEMPBulletTester : MonoBehaviour
+{
+    public GameObject prefabToSpawn;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefabToSpawn, transform);
+        }
+    }
+
+}
