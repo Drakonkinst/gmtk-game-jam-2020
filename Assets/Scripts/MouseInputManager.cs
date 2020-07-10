0819a1667c4c300c1ead3d8a01bbc11de50e80b9
leauxgan1 <logan.langdon@yahoo.com>
2020-07-10 14:17:46 -0700

    Organized materials, created a bullet prefab, and created mouse input manager to read mouse position

---
+++ Assets/Scripts/MouseInputManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Vector2 mousePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mousePosition = MouseEventBase<T0>.mousePosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = MouseEventBase<T0>.mousePosition; // picked up by player to use Transform.lookAt to get the proper rotation
+        Debug.Log("Rotation: " + mousePosition);
+    }
+}
