ef67a53bd0d5f4a623b632b67683b56ebee0e35d
Drakonkinst <drakonkinstmc@gmail.com>
2020-07-10 15:04:17 -0700

    Camera follows Chicken

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform following;
+    public float offset = 6;
+    public float height = 10;
+    
+    private Transform myTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myTransform.position = following.position + new Vector3(-offset, height, -offset);
+    }
+}
