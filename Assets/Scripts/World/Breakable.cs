d670730b017b613c195545dc01b39c0680da083a
Drakonkinst <drakonkinstmc@gmail.com>
2020-07-11 12:24:19 -0700

    Added Breakable component

---
+++ Assets/Scripts/World/Breakable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    public int health = 3;
+    
+    // return health after
+    public int Damage(int howMuch = 1) {
+        Debug.Log("Damaged!");
+        health -= howMuch;
+        if(health <= 0) {
+            Destroy(gameObject);
+            return 0;
+        }
+        return health;
+    }
+}
