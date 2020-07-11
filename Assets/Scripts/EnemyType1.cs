b284fecd363475a039ee426c3b1faf0fa8e7dcb4
Drakonkinst <drakonkinstmc@gmail.com>
2020-07-10 20:08:45 -0700

    Added Enemy type that tracks the player

---
+++ Assets/Scripts/EnemyType1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyType1 : Steerable
+{
+    public float minTrackingDistance = 2.0f;
+    public float slowingDistance = 1.0f;
+    
+    public override void DoBehavior() {
+        float distance = Vector3.Distance(myTransform.position, SceneManager.Instance.playerTransform.position);
+        if(distance <= minTrackingDistance) {
+            FollowPlayer();
+        } else {
+            DoWanderBehavior();
+        }
+    }
+    
+    private void DoWanderBehavior() {
+        steering.Wander();
+    }
+    
+    private void FollowPlayer() {
+        steering.Seek(SceneManager.Instance.playerTransform.position, slowingDistance);
+    }
+}
