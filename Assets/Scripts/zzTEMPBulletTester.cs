7a45e061a5ae5482e7c19012aea4f4061466d517
leauxgan1 <logan.langdon@yahoo.com>
2020-07-10 22:44:30 -0700

    Added 2d inaccuracy to bullets

--- Assets/Scripts/zzTEMPBulletTester.cs
+++ Assets/Scripts/zzTEMPBulletTester.cs
@@ -6,7 +6,9 @@ public class zzTEMPBulletTester : MonoBehaviour
 {
     public GameObject prefabToSpawn;
     //public Transform parentObject;
+    public float inaccuracy = 5.0f;
     private Transform myTransform;
+
     
     // Start is called before the first frame update
     void Start()
@@ -18,7 +20,7 @@ public class zzTEMPBulletTester : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Instantiate(prefabToSpawn, myTransform.position + new Vector3(0.0f, 0.25f, 0.0f), myTransform.rotation); // spawns a bullet at the object and 
+            Instantiate(prefabToSpawn, myTransform.position + new Vector3(0.0f, 0.25f, 0.0f), myTransform.rotation * Quaternion.Euler(0f,Random.Range(-inaccuracy,inaccuracy),0f)); // spawns a bullet at the object and 
             //GameObject bullet = Instantiate(prefabToSpawn, parentObject);
             //bullet.transform.position = gunTransform.position;
         }
