e72f37e5a55ad48f24f78bdf18c7a5c072e6d918
Drakonkinst <drakonkinstmc@gmail.com>
2020-07-10 20:07:58 -0700

    Added Steerable base class for all wandering objects

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,53 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : Steerable
 {
-    public SteeringManager steering;
-    
-    private Transform myTransform;
-    private Transform model;
-    private bool isAvoiding = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Debug.Log("Start");
-        steering = GetComponent<SteeringManager>();
-        if(steering == null) {
-            Debug.LogError("No steering manager found!");
-        }
-        myTransform = transform;
-        model = myTransform.Find("ChickenModel");
-        if(model == null) {
-            Debug.LogError("No player model found!");
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        DoBoundaryBehavior();
-        if(!isAvoiding) {
-            DoWanderBehavior();
-        }
-        
-        model.rotation = Quaternion.Euler(0, -steering.facing * Mathf.Rad2Deg, 0);
-        steering.UpdateSteering();
-    }
-    
-    private void DoBoundaryBehavior() {
-        bool isOutOfBounds = !steering.CheckBounds();
-        if(isOutOfBounds) {
-            isAvoiding = true;
-            steering.Seek(SceneManager.Instance.worldCenter, 0.0f);
-        } else if(isAvoiding) {
-            // not out of bounds
-            isAvoiding = false;
-            steering.SetWanderAngle(SceneManager.Instance.worldCenter);
-        }
-    }
-    
-    private void DoWanderBehavior() {
+    public override void DoBehavior() {
         steering.Wander();
     }
 }
---
+++ Assets/Scripts/Steerable.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steerable : MonoBehaviour
+{
+    public string modelName = "ChickenModel";
+    [System.NonSerialized]
+    public SteeringManager steering;
+    [System.NonSerialized]
+    public Transform myTransform;
+    
+    private Transform model;
+    private bool isAvoiding = false;
+    private Vector3 lastGoodPosition;
+
+    void Start()
+    {
+        myTransform = transform;
+        steering = GetComponent<SteeringManager>();
+        if(steering == null) {
+            Debug.LogError("No steering manager found!");
+        }
+        model = myTransform.Find(modelName);
+        if(model == null) {
+            Debug.LogError("No steerable model found!");
+        }
+    }
+
+    void Update()
+    {
+        if(!steering.IsValid(SteeringManager.ToVector2(myTransform.position))) {
+            myTransform.position = lastGoodPosition;
+        } else {
+            lastGoodPosition = myTransform.position;
+        }
+        DoBoundaryBehavior();
+        if(!isAvoiding) {
+            DoBehavior();
+        }
+        
+        model.rotation = Quaternion.Euler(0, -steering.facing * Mathf.Rad2Deg, 0);
+        steering.UpdateSteering();
+    }
+    
+    private void DoBoundaryBehavior() {
+        
+        bool isOutOfBounds = !steering.CheckBounds();
+        if(isOutOfBounds) {
+            isAvoiding = true;
+            steering.Seek(SceneManager.Instance.worldCenter, 0.0f);
+        } else if(isAvoiding) {
+            // not out of bounds
+            isAvoiding = false;
+            steering.SetWanderAngle(SceneManager.Instance.worldCenter);
+        }
+    }
+    
+    public virtual void DoBehavior() {
+        steering.Wander();
+    }
+}
