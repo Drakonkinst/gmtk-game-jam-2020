3f5f65bf7941d9ae540a54023c4ab2481e1757a9
Drakonkinst <drakonkinstmc@gmail.com>
2020-07-11 17:54:23 -0700

    What the fuck

--- Assets/Scripts/Mobs/PlayerMovement.cs
+++ Assets/Scripts/Mobs/PlayerMovement.cs
@@ -27,4 +27,8 @@ public class PlayerMovement : Steerable
         steering.SetWanderAngleToFacing();
         isFleeing = false;
     }
+    
+    void OnTriggerStay(Collider other) {
+        Debug.Log("Hit! " + other.tag);
+    }
 }
