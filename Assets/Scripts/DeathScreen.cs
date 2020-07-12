82583d5dc9ae67d771804a6fc93bef236eb743a4
Drakonkinst <drakonkinstmc@gmail.com>
2020-07-11 21:41:29 -0700

    Added Death Screen

---
+++ Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DeathScreen : MonoBehaviour
+{
+    public GameObject scoreText;
+    
+    private void UpdateScore(int value) {
+        scoreText.GetComponent<TextMeshProUGUI>().SetText("Score: " + value);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        int score = PlayerPrefs.GetInt("player_score");
+        UpdateScore(score);
+        
+        //TODO play death sound
+    }
+}
